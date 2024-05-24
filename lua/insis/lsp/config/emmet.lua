99f37354711af0da7f4c75ad667830a18e8c269a
chixm <imoshanghua1314@outlook.com>
2024-05-24 23:04:44 +0800

    fix:两个无害的警告

--- lua/insis/lsp/config/emmet.lua
+++ lua/insis/lsp/config/emmet.lua
@@ -3,6 +3,22 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 local opts = {
   capabilities = capabilities,
   filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+  --  调整初始化选项和设置,消除一些无害的警告
+  init_options = {
+    html = {
+      options = {
+        ["bem.enabled"] = true,
+      },
+    },
+  },
+  settings = {
+    emmet = {
+      showExpandedAbbreviation = "always",
+      showSuggestionsAsSnippets = true,
+      syntaxProfiles = {},
+      variables = {},
+    },
+  },
 }
 return {
   on_setup = function(server)
--- lua/insis/lsp/config/html.lua
+++ lua/insis/lsp/config/html.lua
@@ -14,6 +14,23 @@ local opts = {
     },
     provideFormatter = true,
   },
+  -- 确认validProperties 被适当地设置，避免 null 引起的错误
+  settings = {
+    html = {
+      format = {
+        enable = true,
+      },
+      validate = {
+        styles = true,
+        scripts = true,
+      },
+    },
+    css = {
+      lint = {
+        validProperties = {}, -- 确保这个属性设置了默认值
+      },
+    },
+  },
 }
 return {
   on_setup = function(server)
