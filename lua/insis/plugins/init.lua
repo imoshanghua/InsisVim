0c881e8e19d1d3313bc303e06e804ac0306ec4b2
chixm <imoshanghua1314@outlook.com>
2024-09-30 09:16:49 +0800

    feat: Add multi-cursor editing

--- lua/insis/plugins/init.lua
+++ lua/insis/plugins/init.lua
@@ -179,6 +179,8 @@ return {
     end,
   },
 
+  { "mg979/vim-visual-multi" },
+
   ------------------ Markdown -------------------------------------------------
   {
     "jakewvincent/mkdnflow.nvim",
